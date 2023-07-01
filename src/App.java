2f38236d8be5eabe72b1185d3ab1b0994e11b698
Emil <emil88pl@gmail.com>
2023-07-01 11:40:20 +0100

    Deserialization 2 users

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 import java.io.FileInputStream;
 import java.io.ObjectInputStream;
+import java.io.ObjectStreamClass;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -14,22 +15,41 @@ public class App {
         // 6 objectName = (Class) in.readObject();
         // 7 in.close(); fileIn.close();
 
+        // Notes to remember
 
-        User user = null;
+        // 1 Children classes of a parent class that implements Serializable will do so as well
+        // 2 static fields are not serialized (they belong to the class, not an individual object)
+        // 3 the class's definition ("class file") itself is not recorded, cast it as the object type
+        // 4 Fields declared as "transient" are not serialized, they are ignored
+        // 5 sierialVersionUID is a unique version ID for class that is serializable
+
+
+
+        User user1 = null;
+        User user2 = null;
+       
 
         FileInputStream fileIn = new FileInputStream("C:/JAVA 23''/First test in 23''/Serializer/UserInfo.ser");
         ObjectInputStream in = new ObjectInputStream(fileIn);
         
-        user = (User) in.readObject();
+        user1 = (User) in.readObject();
+        user2 = (User) in.readObject();
+       
 
         in.close();
         fileIn.close();
 
 
-        System.out.println(user.name);
-        System.out.println(user.password);
-        user.seyHello();
+        System.out.println(user1.name);
+        System.out.println(user1.password);
+        user1.seyHello();
 
+        System.out.println(user2.name);
+        System.out.println(user2.password);
+        user2.seyHello();
+
+        long serialVersionUID = ObjectStreamClass.lookup(user1.getClass()).getSerialVersionUID();
+        System.out.println(serialVersionUID);
 
     }
 }
--- src/User.java
+++ src/User.java
@@ -3,7 +3,7 @@ import java.io.Serializable;
 public class User  implements Serializable {
     
     String name;
-    String password;
+    transient String password;
 
     public void seyHello() {
         System.out.println("Hello " + name);
